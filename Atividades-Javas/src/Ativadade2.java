750bc5ba10f99de68bdc36c8eef108ba2cdbb90d
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-05-22 20:26:45 -0300

    a

--- Atividades-Javas/src/Ativadade2.java
+++ Atividades-Javas/src/Ativadade2.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Ativadade2 {
     public static void main(String[] args) {
         
-    }
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        int temp[];
+        int n = 0;
+        int total = 0;
+
+
+
+        System.out.print("Escreva um numero para o tamanho do Vetor: ");
+        n = scanner.nextInt();
+        temp = new int[n];
+
+        for (i = 0; i < n; i++) {
+            System.out.print("Escreva numeros:");
+            temp[i] = scanner.nextInt();
+        }
+        for (i = 0; i < n; i++) {
+            System.out.println("Seus vetores foram: "+temp[i]);
+        }
+
+        for (i = 0; i < n ; i++) {
+
+                temp[i] = total;
+
+                temp[i] = temp[i];
+
+                System.out.println(total+""+temp[i]);
+            }
+
+        }
+        
 }
+
