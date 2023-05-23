69eea24fe1ac0168ae52638cea46d08dcd00dd67
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-05-22 22:04:03 -0300

    aa

--- Atividades-Javas/src/Atividade1.java
+++ Atividades-Javas/src/Atividade1.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Atividade1 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int i;
+        int temp[];
+        int n = 0;
+        temp = new int[5];
+
+        for (i = 0; i < 5 ; i++) {
+            System.out.print("Digite 5 numeros:");
+            temp[i] = scanner.nextInt();
+            
+        }
     }
 }
