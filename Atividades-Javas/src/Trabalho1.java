97ea0829c161a1b640cce6e35c3612410186166f
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-05-26 22:06:30 -0300

    ]

--- Atividades-Javas/src/Trabalho1.java
+++ Atividades-Javas/src/Trabalho1.java
@@ -4,12 +4,16 @@ public class Trabalho1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int cardapio[];
-        int i = 0;
+        String enter ="o";
         int resposta =0;
+        
+        int cardapio[];
 
+        int n = 0;
 
-        while (resposta != 0){
+        cardapio = new int[n];
+
+        while (resposta != 5){
 
             System.out.println("--------------------------------------------------");
             System.out.println("|          O que você gostaria de fazer?         |");
@@ -20,15 +24,46 @@ public class Trabalho1 {
             System.out.println("|           4 - Buscar item por número           |");
             System.out.println("|                    5 - sair                    |");
             System.out.println("--------------------------------------------------");
+            System.out.print("Resposta..:");
             resposta = scanner.nextInt();
             System.out.println("");
-            System.out.println("--------------------------------------------------") 
+            System.out.println("--------------------------------------------------");
+
+            if ( resposta == 1){
+                int j = 1; 
+                System.out.println("");
+                System.out.println("Cardápio:");
+                System.out.println("");
+
+               for (int i = 0; i < n ; i++) {
+
+                System.out.println(cardapio[i]);   
+               }
+            System.out.println();
+            System.out.println("--------------------------------------------------");
+            System.out.println();
+            System.out.println("           Pressione <Enter> para voltar          ");
+            System.out.println();
+            System.out.println("--------------------------------------------------");
+            System.out.println("Gostaria de repetir? Sim ou não");
+            enter = scanner.next();
+            
+
+            
+            } 
+            }
+            
+
+            
+
+
+
+            
 
         }
-        System.out.println();
+      
         
 
 
 
     }
-}
