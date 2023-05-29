a77beec6b0a2da0b19df76ea044746d3daf4749a
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-05-29 20:41:00 -0300

    c

--- Atividades-Javas/src/Trabalho1.java
+++ Atividades-Javas/src/Trabalho1.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 
+import javax.swing.text.AbstractDocument.BranchElement;
+
 public class Trabalho1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String enter = "o";
-        int resposta = 0;
-        int n = 1;
-        int[] cardapio = new int[n];
         
+        int resposta = 0;
+        int n = 0;
+        int lista[];
+        int i = 0;
+        String enter = "";
 
+        System.out.print("Escreva um numero para o tamanho do Vetor: ");
+        n = scanner.nextInt();
+        lista = new int[n];
         while (resposta != 5) {
             
-            n = n + 1;
+            
+            
             System.out.println("--------------------------------------------------");
             System.out.println("|          O que você gostaria de fazer?         |");
             System.out.println("--------------------------------------------------");
@@ -20,32 +27,27 @@ public class Trabalho1 {
             System.out.println("|                2 - remover item                |");
             System.out.println("|              3 - adicionar um item             |");
             System.out.println("|           4 - Buscar item por número           |");
+            System.out.println("|          4 - Aumentar o tamanho máx da Lista   |");
             System.out.println("|                    5 - sair                    |");
             System.out.println("--------------------------------------------------");
             System.out.print("Resposta..:");
             resposta = scanner.nextInt();
             System.out.println("");
             System.out.println("--------------------------------------------------");
-            switch (resposta) {
-                case 1:
-                System.out.println("Cardápio");
-                for (int i = 0; i < cardapio.length; i++) {
-                    System.out.println(cardapio);
-                }
-                System.out.println("Precione qual quer coisa para voltar!");
-                scanner.next();
-
-                case 3:
-
-                System.out.println("Digite o numero a ser adicionado: ");
-                int num = scanner.nextInt();
-                cardapio[i] = num;
-                System.out.println("Precione qual quer coisa para voltar!");
-                scanner.next();
+            
+                if (resposta == 1){
+                        System.out.println(lista[i]);
+                
             }
-
-             
-
+                else if (resposta == 3){
+                    System.out.print("Escreva o numero "+i+" da lista: ");
+                    lista[i] = scanner.nextInt();
+                }
+                
+            
+            
+            System.out.println("Precione qual quer tecla para continuar");
+            enter = scanner.next();
 
         }
     }
