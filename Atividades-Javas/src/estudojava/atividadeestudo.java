e3889cbbf1ca794490578a3be058415faf94bde5
FilipeAnklanOficial <107378137+FilipeAnklanOficial@users.noreply.github.com>
2023-06-29 19:55:56 -0300

    kjl

--- Atividades-Javas/src/estudojava/atividadeestudo.java
+++ Atividades-Javas/src/estudojava/atividadeestudo.java
@@ -1,8 +1,16 @@
 package estudojava;
 
-public class atividadeestudo {
-    static void oi(String[][] matriz){
-        System.out.println("oi");
+import java.util.Scanner;
 
+public class atividadeestudo {
+    Scanner scanner = new Scanner(System.in);
+    public static void oi(String[][] matriz, int oi){
+        
     }
+    public static void main(String[] args) {
+        oi(1, );
+    }
+    
+    
+
 }
