c6641e825988ed1176f3ad1bc42477b57c6d829f
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2023-07-05 17:46:37 -0300

    egsdf

--- Atividades-Javas/src/estudojava/estudo.java
+++ Atividades-Javas/src/estudojava/estudo.java
@@ -1,31 +1,39 @@
 package estudojava;
 import java.util.Scanner;
 public class estudo {
-    public static void main(String[] args){
+    public static void tudo(int l, int c){
+        Scanner scanner = new Scanner(System.in);
+        String[][] matrix;
+        matrix = new String[l][c];
         int i = 0;
         int j = 0;
-        String[][] matrix;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Escreva a quantidade de linhas da matrix: ");
-        int l = scanner.nextInt();
-        System.out.print("Agora escreva a quantidade de colunas da matrix: ");
-        int c = scanner.nextInt();
-        matrix = new String[l][c];
+        int num = 0;
+        int num2 = 1;
         for (i = 0; i < l; i++){
-            System.out.print("Escreva o que vai ser colocado na primeira linha: ");
-            matrix[i][j] = scanner.nextLine();
+            num +=1;
             for (j = 0; j < c;j++){
-                int num = 1;
-                System.out.print("Escreva o que vai aparecer na "+num+"° coluna da linha");
-                matrix[i][j] = scanner.nextLine();
-                num +=1;
+                System.out.print("Escreva o que vai aparecer na linha "+num+" coluna "+num2+": ");
+                matrix[i][j] = scanner.next();
+                num2 +=1;
             }
+            num2 = 1;
+        }
         for (i = 0; i < l; i++){
-            for (j=0;j<c;j++){
+            for (j=0; j < c; j++){
                 System.out.print(matrix[i][j]);
             }
             System.out.print("\n");
         }
-        }
     }
+    public static void main(String[] args){
+        int l =0;
+        int c = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Escreva a quantidade de linhas da matrix: ");
+        l = scanner.nextInt();
+        System.out.print("Agora escreva a quantidade de colunas da matrix: ");
+        c = scanner.nextInt();
+        tudo(l, c);
+    }
+    
 }
